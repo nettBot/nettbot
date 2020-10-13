90bc97145ecf2d5b505964b292ab9d1acd18839c
ZenuxVI <mobile.thomasjp@gmail.com>
2020-10-13 18:14:09 +0200

    Added my project

---
+++ libs/functions/worker.js
@@ -0,0 +1,14 @@
+const a = require("debug")("worker:a"),
+  b = require("debug")("worker:b"),
+  c = require("debug")("worker:c"),
+  d = require("debug")("worker:d");
+
+async function work(worker, description) {
+  let worker_a = a;
+  let worker_b = b;
+  let worker_c = c;
+
+  worker = async(description) = {
+   
+  };
+}
